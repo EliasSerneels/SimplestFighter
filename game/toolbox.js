e8922e4e943c63792833130e312b3f5cd1fd32da
EliasSerneels <r0381171@student.thomasmore.be>
2015-02-13 21:49:13 +0100

    Collision with containment

--- game/toolbox.js
+++ game/toolbox.js
@@ -707,11 +707,17 @@ TOOLBOX.CollisionDetection2D = {
 	// Axis-Aligned Bounding Box
 	// Checks if 2 axis-aligned rectangles are overlapping
 	// TODO: Return resulting vector
-	AABB : function(r1, r2) {
-		if(r1.x < r2.x + r2.width && r1.x + r1.width > r2.x && r1.y < r2.y + r2.height && r1.height + r1.y > r2.y) {
-			return true;
+	AABBCollision : function(r1, r2) {
+			return (r1.x < r2.x + r2.width && r1.x + r1.width > r2.x && r1.y < r2.y + r2.height && r1.height + r1.y > r2.y);
+	}
+	
+	AABBContainment : function(r1, r2){
+		if(r1.x+r1.width>=r2.width{
+			r1.x=r1.x-r1.width;
 		}
-		return false;
+		if(r1.y+r1.height>=r2.width){
+			r1.y=r1.y-r1.height;
+		}
 	}
 }
 
