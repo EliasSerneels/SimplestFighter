d5f350f346dab16ead27486f52913b655bd3b496
Janne Siera <janne.siera@gmail.com>
2015-02-13 19:21:49 +0100

    Game setup

--- game/game.js
+++ game/game.js
@@ -8,7 +8,7 @@ window.onload = init;
 function init() {
 	// srcArray and path
 	var path = 'img/';
-	var srcArray = ['circle.png', 'rectangle.png', 'triangle.png'];
+	var srcArray = ['rectangle1.png', 'rectangle2.png'];
 	
 	// Create the tools
 	GAME.Engine = new TOOLBOX.Engine(30, updateGame);
@@ -16,19 +16,6 @@ function init() {
 	GAME.EventHandler = new TOOLBOX.EventHandler(GAME.Renderer);
 	GAME.AssetManager = new TOOLBOX.AssetManager(path, srcArray);
 	GAME.AssetManager.load();
-	
-	// Asset management
-	GAME.FileStreamProvider = new TOOLBOX.StreamProviders.File("assets");	
-	GAME.AssetLoader = new TOOLBOX.AssetLoader(GAME.FileStreamProvider);
-	GAME.AssetLoader.registerType("AnimationXML", new TOOLBOX.AssetLoaders.AnimationXML());
-	GAME.AssetCache = new TOOLBOX.AssetCache(GAME.AssetLoader);
-	
-	GAME.AssetCache.get("AnimationXML", "test.xml", function(animation) {
-		//
-
-	});
-	
-	
 	
 	// Create Entity Manager and register all component types
 	GAME.EntityManager = new TOOLBOX.EntityManager();
@@ -41,7 +28,7 @@ function init() {
 	GAME.EntityManager.registerComponentType('CCollision');
 	
 	// Create a group for basic collisions
-	GAME.EntityManager.createGroup('COLLISION_BODIES');
+	GAME.EntityManager.createGroup('COLLISION_BODIES_PLAYERS');
 	
 	// Set entity manager for Systems
 	SMove.setEntityManager(GAME.EntityManager);
@@ -49,64 +36,40 @@ function init() {
 	SRender.setEntityManager(GAME.EntityManager);
 	SCollisionDetection.setEntityManager(GAME.EntityManager);
 	SCollisionResolver.setEntityManager(GAME.EntityManager);
-	
-	// Create camera (pos, square, tag)
-	var camera = GAME.EntityManager.create();
-	GAME.EntityManager.addComponent(camera, new CPos(0, 0))
-		.addComponent(camera, new CSquare(500, 500))
-		.addTag(camera, 'CAMERA');
 
-	// Create an entity
+	// Create player 1
 	var polygon = new TOOLBOX.Polygon();
 	polygon.addVertex(0, 0);
 	polygon.addVertex(50, 0);
-	polygon.addVertex(50, 50);
-	polygon.addVertex(0, 50);
-	polygon.setCenter(25, 25);
+	polygon.addVertex(50, 75);
+	polygon.addVertex(0, 75);
+	polygon.setCenter(25, 37.5);
 	
 	var e = GAME.EntityManager.create();
-	GAME.EntityManager.addComponent(e, new CPos(50, 50))
+	GAME.EntityManager.addComponent(e, new CPos(100, 300))
+	GAME.EntityManager.addComponent(e, new CVelocity(0, 0));
+	GAME.EntityManager.addComponent(e, new CAccel(60));
+	GAME.EntityManager.addComponent(e, new CRender(0));
+	GAME.EntityManager.addComponent(e, new CShape('poly', polygon));
+	GAME.EntityManager.addToGroup(e, 'COLLISION_BODIES_PLAYERS');
+	GAME.EntityManager.addTag(e, 'PLAYER1');
+	
+	// Create player 2
+	var polygon = new TOOLBOX.Polygon();
+	polygon.addVertex(0, 0);
+	polygon.addVertex(50, 0);
+	polygon.addVertex(50, 75);
+	polygon.addVertex(0, 75);
+	polygon.setCenter(25, 37.5);
+	
+	var e = GAME.EntityManager.create();
+	GAME.EntityManager.addComponent(e, new CPos(100, 300))
 	GAME.EntityManager.addComponent(e, new CVelocity(0, 0));
 	GAME.EntityManager.addComponent(e, new CAccel(60));
 	GAME.EntityManager.addComponent(e, new CRender(1));
 	GAME.EntityManager.addComponent(e, new CShape('poly', polygon));
-	GAME.EntityManager.addToGroup(e, 'COLLISION_BODIES');
-	GAME.EntityManager.addTag(e, 'PLAYER');
-	
-	// Create an entity
-	var polygon = new TOOLBOX.Polygon();
-	polygon.addVertex(0, 0);
-	polygon.addVertex(50, 0);
-	polygon.addVertex(50, 50);
-	polygon.addVertex(0, 50);
-	polygon.setCenter(25, 25);
-	
-	var e = GAME.EntityManager.create();
-	GAME.EntityManager.addComponent(e, new CPos(100, 100));
-	GAME.EntityManager.addComponent(e, new CRender(1));
-	GAME.EntityManager.addComponent(e, new CShape('poly', polygon));
-	GAME.EntityManager.addToGroup(e, 'COLLISION_BODIES');
-	
-	// Create an entity
-	var polygon = new TOOLBOX.Polygon();
-	polygon.addVertex(25, 0);
-	polygon.addVertex(50, 50);
-	polygon.addVertex(0, 50);
-	polygon.setCenter(25, 25);
-	
-	var e = GAME.EntityManager.create();
-	GAME.EntityManager.addComponent(e, new CPos(200, 200));
-	GAME.EntityManager.addComponent(e, new CRender(2));
-	GAME.EntityManager.addComponent(e, new CShape('poly', polygon));
-	GAME.EntityManager.addToGroup(e, 'COLLISION_BODIES');
-	
-	// Create a canvas
-	/*
-	var e = GAME.EntityManager.create();
-	GAME.EntityManager.addComponent(e, new CPos(0, 0));
-	GAME.EntityManager.addComponent(e, new CSquare(GAME.Renderer.getWidth, GAME.Renderer.getHeight)); // This must be the height and width of the screen in a simple game
-	GAME.EntityManager.addTag(e, 'WORLD');
-	*/
+	GAME.EntityManager.addToGroup(e, 'COLLISION_BODIES_PLAYERS');
+	GAME.EntityManager.addTag(e, 'PLAYER2');
 	
 	// Start the tools
 	GAME.EventHandler.startEventHandler();
@@ -115,10 +78,9 @@ function init() {
 
 // Update function called eahc frame
 function updateGame() {
-	SAccelChar.process(GAME.EventHandler, GAME.Engine.getDeltaTime());
-	SMove.process();
-	SCollisionDetection.process(GAME.EntityManager.getGroup('COLLISION_BODIES'));
-	SCollisionResolver.process();
+	// SAccelChar.process(GAME.EventHandler, GAME.Engine.getDeltaTime());
+	// SMove.process();
+	// SCollisionDetection.process(GAME.EntityManager.getGroup('COLLISION_BODIES'));
 	SRender.process(GAME.Renderer, GAME.AssetManager);
 }
 
