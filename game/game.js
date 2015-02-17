65804c186a4113768866f8f7f69516d06090b763
EliasSerneels <r0381171@student.thomasmore.be>
2015-02-17 22:06:40 +0100

    Fixed double entry

--- game/game.js
+++ game/game.js
@@ -81,7 +81,6 @@ function updateGame() {
 	SMove.process(GAME.Engine.getDeltaTime());
 	STouchGround.process(GAME.EntityManager.getGroup('COLLISION_BODIES_PLAYERS'), 800);
 	STouchWall.process(GAME.EntityManager.getGroup('COLLISION_BODIES_PLAYERS'), 800);
-	SDash.process(GAME.Engine.getDeltaTime(), 500);
 	SDash.process(GAME.Engine.getDeltaTime(), 100, 1200);
 	SCoolDown.process(GAME.Engine.getDeltaTime());
 	
